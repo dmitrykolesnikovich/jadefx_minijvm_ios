7d81f6f9edc5b573c2185187679cc5bc1c3c6924
Andrew Hamilton <ahamilton@manh.com>
2021-07-10 11:45:29 -0400

    Initial Upload

---
+++ c/glfmapp/main.c
@@ -0,0 +1,83 @@
+// Example app that draws a triangle. The triangle can be moved via touch or keyboard arrow keys.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "glfm.h"
+
+
+#define FILE_COMPAT_ANDROID_ACTIVITY glfmAndroidGetActivity()
+
+
+#include "jvm.h"
+#include "jvm_util.h"
+#include "media.h"
+
+#ifdef NDEBUG
+#define LOG_DEBUG(...) do { } while (0)
+#else
+#if defined(GLFM_PLATFORM_ANDROID)
+#define LOG_DEBUG(...) __android_log_print(ANDROID_LOG_INFO, "GLFM", __VA_ARGS__)
+#else
+#define LOG_DEBUG(...) do { } while (0)
+#endif
+#endif
+
+extern void JNI_OnLoad_mini(MiniJVM *jvm);
+extern void JNI_OnUnload_mini(MiniJVM *jvm);
+static GLFMDisplay *glfm_display;
+
+// Main entry point
+void glfmMain(GLFMDisplay *display) {
+    glfm_display=display;
+    
+    //init refers ,the globle var
+    memset(&refers, 0, sizeof(GlobeRefer));
+
+    Utf8String *bootclasspath = utf8_create();
+    utf8_append_c(bootclasspath, (char*)glfmGetResRoot());
+    utf8_append_c(bootclasspath, "/resfiles/minijvm_rt.jar;");
+    utf8_append_c(bootclasspath, (char*)glfmGetResRoot());
+    utf8_append_c(bootclasspath, "/resfiles/minijvm_extras.jar;");
+    utf8_append_c(bootclasspath, (char*)glfmGetResRoot());
+    utf8_append_c(bootclasspath, "/resfiles/joml-1.9.25.jar;");
+    
+    Utf8String *classpath = utf8_create();
+    utf8_append_c(classpath, (char*)glfmGetResRoot());
+    utf8_append_c(classpath, "/resfiles/glfm_gui.jar;");
+    utf8_append_c(classpath, (char*)glfmGetResRoot());
+    utf8_append_c(classpath, "/resfiles/JadeFX.jar;");
+    utf8_append_c(classpath, (char*)glfmGetResRoot());
+    utf8_append_c(classpath, "/resfiles/ExApp.jar;");
+    
+    jvm_printf("\nClass path: %s\n",utf8_cstr(classpath));
+
+    refers.jvm = jvm_create();
+    if(!refers.jvm){
+        jvm_printf("[ERROR] jvm create error.\n");
+        return;
+    }
+    refers.jvm->jdwp_enable = 0; //set to 1 if enable jdwp for java debug
+    s32 ret = jvm_init(refers.jvm, utf8_cstr(bootclasspath), utf8_cstr(classpath));
+    if(ret){
+        jvm_printf("[ERROR] jvm init error.\n");
+        return ;
+    }
+    JNI_OnLoad_mini(refers.jvm);
+    sys_properties_set_c(refers.jvm, "glfm.res.root", (char*)glfmGetResRoot());
+    sys_properties_set_c(refers.jvm, "glfm.save.root", (char*)glfmGetSaveRoot());
+    sys_properties_set_c(refers.jvm, "glfm.uuid", (char*)glfmGetUUID());
+    Runtime *runtime=getRuntimeCurThread(&jnienv);
+
+    utf8_destory(classpath);
+    utf8_destory(bootclasspath);
+    c8* p_classname="org/mini/glfm/GlfmCallBackImpl";
+    c8* p_methodname="glinit";
+    c8* p_methodtype="(J)V";
+    push_long(runtime->stack,(s64)(intptr_t)display);
+    call_method(refers.jvm, p_classname,p_methodname,p_methodtype,runtime);
+}
+
+void glfmDestroy() {
+    JNI_OnUnload_mini(refers.jvm);
+    jvm_destroy(refers.jvm);
+}
---
+++ c/gui/media.c
@@ -0,0 +1,69 @@
+//
+// Created by Gust on 2018/2/1.
+//
+
+#include "jvm_util.h"
+#include "jvm.h"
+#include "media.h"
+
+
+void JNI_OnLoad_mini(MiniJVM *jvm) {
+    JniEnv *env = jvm->env;
+    refers.env = env;
+
+    refers.runtime_list=pairlist_create(10);
+
+
+    env->native_reg_lib(jvm, ptr_GlfmFuncTable(), count_GlfmFuncTable());
+    env->native_reg_lib(jvm, ptr_GLFuncTable(), count_GLFuncTable());
+    env->native_reg_lib(jvm, ptr_NanovgFuncTable(), count_NanovgFuncTable());
+    env->native_reg_lib(jvm, ptr_MiniALFuncTable(), count_MiniALFuncTable());
+}
+
+void JNI_OnUnload_mini(MiniJVM *jvm) {
+    JniEnv *env = jvm->env;
+    env->native_remove_lib(jvm, ptr_GlfmFuncTable());
+    env->native_remove_lib(jvm, ptr_GLFuncTable());
+    env->native_remove_lib(jvm, ptr_NanovgFuncTable());
+
+    s32 i;
+    for(i=0;i<refers.runtime_list->count;i++){
+        Runtime *runtime=pairlist_get_pair(refers.runtime_list, i).right;
+        if(runtime){
+            thread_unboundle(runtime);
+            runtime_destory(runtime);
+        }
+    }
+    pairlist_destory(refers.runtime_list);
+    
+}
+
+tss_t TSS_KEY_RUNTIME_OF_THREAD;
+Runtime *getRuntimeCurThread(JniEnv *env) {
+    static s32 init=0;
+    if(!init){
+        tss_create(&TSS_KEY_RUNTIME_OF_THREAD,NULL);
+        //tss_create(&TSS_KEY_RUNTIME_OF_THREAD,(tss_dtor_t)env->runtime_destory);
+        init = 1;
+    }
+    if (env->get_jvm_state(refers.jvm) != JVM_STATUS_RUNNING) {
+        return NULL;
+    }
+    thrd_t t = env->thrd_current();
+    Runtime *runtime = tss_get(TSS_KEY_RUNTIME_OF_THREAD);
+    if (!runtime) {
+        runtime = env->runtime_create(refers.jvm);
+        env->thread_boundle(runtime);
+        env->jthread_set_daemon_value(runtime->thrd_info->jthread, runtime, 1);
+        tss_set(TSS_KEY_RUNTIME_OF_THREAD,runtime);
+    }
+    
+    return runtime;//
+}
+
+/* ===============================================================
+ *
+ *                          DEMO
+ *
+ * ===============================================================*/
+
